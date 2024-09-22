867f875d9d0e6eec71353411a57e70925289e639
Isaac <iico7no2@gmail.com>
2024-09-21 23:40:30 -0500

    Update

--- scripts/workflows/package-version/package-version.js
+++ scripts/workflows/package-version/package-version.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const core = require('@actions/core');
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json'));
 core.setOutput('value', packageJson.version);
