95da03c6cbf663305834a253064a3e0e9455dc00
Isaac <iico7no2@gmail.com>
2024-09-22 00:19:58 -0500

    Adjust

--- scripts/workflows/verify-web-artifacts/verify-web-artifacts.js
+++ scripts/workflows/verify-web-artifacts/verify-web-artifacts.js
@@ -2,3 +2,12 @@ const allowedWebBranches = input('allowedWebBranches', '');
 
 const aaa = JSON.parse(allowedWebBranches);
 console.log(aaa);
+
+function input(name, def) {
+  let inp = core.getInput(name).trim();
+  if (inp === '' || inp.toLowerCase() === 'false') {
+    return def;
+  }
+
+  return inp;
+}
