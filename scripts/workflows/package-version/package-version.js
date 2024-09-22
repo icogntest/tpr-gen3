ae2054640757ebe0eca68027ee40e86fe413397e
Isaac <iico7no2@gmail.com>
2024-09-21 23:31:19 -0500

    Update

--- scripts/workflows/package-version/package-version.js
+++ scripts/workflows/package-version/package-version.js
@@ -1,8 +1,12 @@
+const fs = require('node:fs');
 // const { version } = require('package.json');
 
 (async () => {
   console.log(`cwd:${process.cwd()}`);
   // process.cwd();
+  const fileList = fs.readdirSync(directoryPath);
+
+  console.log('Files and folders in the directory:', fileList);
 
   // core.setOutput('value', version);
 })();
